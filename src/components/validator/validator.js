c5440632db69d17004765423f89606cd37eedd7b
LittleLionMan <sibnie@web.de>
2022-08-16 07:04:32 +0200

    minor fix

--- src/components/validator/validator.js
+++ src/components/validator/validator.js
@@ -1,4 +1,4 @@
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useDispatch } from "react-redux";
 
 import { setVal } from './validatorSlide';
