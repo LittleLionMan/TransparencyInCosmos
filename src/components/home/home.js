428001d63162394f6ffd24d1df33619de27f7af3
LittleLionMan <sibnie@web.de>
2022-08-30 14:39:28 +0200

    first intro draft

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -6,7 +6,11 @@ export function Home() {
     
         <div className='home'>
             <div className='introText' id='pd'>
-                <p>Project description</p>
+                <p id="quote">“those who have a why can bear almost any how.”</p>
+                <p id="author">Friedrich Nietzsche</p>
+                <p>Blockchain has at its core has a revolutionary technology. The ability to see the actions and relationships of all participants of a system in real time. While that data is available for all to see at all times, in practice it is only the most technically savvy among us that can take full advantage of that on chain information.</p>
+                <p>The Cosmos Ecosystem has become one of the leading blockchain environments and for good reason. The overall technology of the Cosmos SDK, that allows for the fast and secure communication of separate blockchains, has proven to be robust and effective. The community that the Cosmos has attracted and developed has also become one of the ecosystem's greatest strengths. The powerful debate culture of the Cosmos Ecosystem has proven to be a great asset in pushing innovation, security and vision.</p>
+                <p>We at the Cosmos Transparency Project seek to better organize the on chain data of the various blockchains linked by the Cosmos SDK to better inform the community about the 'health' of the ecosystem as a whole. We hope to contribute to the community by offering a space where usable information is accessible and easy to understand so that community members will be better able to make effective individual decisions about governance, decentralization and risk management.We at the Cosmos Transparency Project seek to better organize the on chain data of the various blockchains linked by the Cosmos SDK to better inform the community about the 'health' of the ecosystem as a whole. We hope to contribute to the community by offering a space where usable information is accessible and easy to understand so that community members will be better able to make effective individual decisions about governance, decentralization and risk management.</p>
             </div>
             <div className='contactInfo' id='ci'>
                 <a href="https://twitter.com/CosmosTProject"><img src={twitter}  alt="Twitter" id="twitter" /></a>
